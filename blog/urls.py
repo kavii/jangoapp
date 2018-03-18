1426fcdac94ae1039bbc5597c5b74a72d4bcf1c8
kavii <kavita.kat963@gmail.com>
2018-03-18 17:15:52 +0530

    myjangopush

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.conf.urls import url
+from blog import views
+
+urlpatterns = [
+    url(r'^$',views.test, name="test"),
+ ]
---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import myBlog
+# Create your views here.
+
+myPosts = myBlog.object.all() 
+
+myDict = {"htmlVar": myPosts}
+def index(request):
+    return render(request, "index.html")
+
+def test(request):
+    return HttpResponse("<h2>Hello World!</h2>")
